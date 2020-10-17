5f90b276b243c29db6de7d72fe33df28db048e45
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-17 20:55:57 -0300

    Add User Schema

---
+++ src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from 'mongoose'
+
+const UserSchema: Schema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model('User', UserSchema)
