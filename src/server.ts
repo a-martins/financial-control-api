b76830c543d4040a30091a489052cd6ab56e21c6
Alexandre Martins <smartins@daitan.com>
2020-10-17 20:36:04 -0300

    Add mongodb connection

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import connectDB from './config/db'
 
 const app = express()
+
+connectDB()
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(5000)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
