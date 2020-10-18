3ce6c8bb8ce330db10cb10349fa3294306cac076
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-17 22:25:56 -0300

    Add UserController with create method

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,44 @@
+import { Request, Response } from 'express'
+import User from '../models/User'
+import bcrypt from 'bcryptjs'
+import config from 'config'
+import jwt from 'jsonwebtoken'
+
+export default class UserController {
+    async create(request: Request, response: Response) {
+        const { email, firstName, lastName, password, createdAt } = request.body;
+
+        try {
+            let user = await User.findOne({ email })
+
+            if (user) return response.boom.badRequest('User Already Exists')
+
+            const salt = await bcrypt.genSalt(10);
+            const encryptedPassword = await bcrypt.hash(password, salt);
+
+            user = new User({ email, firstName, lastName, password: encryptedPassword, createdAt });
+
+            await user.save()
+
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 360000
+                },
+                (err, token) => {
+                    if (err) throw response.boom.badImplementation(err.message)
+                    response.json({ token })
+                }
+            )
+        } catch (error) {
+            response.boom.badImplementation('Server error')
+        }
+    }
+}
