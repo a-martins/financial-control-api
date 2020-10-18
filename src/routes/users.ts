44862663ee0cfb67c2ed855531ce3062770f42b9
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-18 17:31:12 -0300

    Remove redundant error messages

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,12 +7,10 @@ const routes = express.Router();
 const userControler = new UserControler()
 
 routes.post('/', [
-    check('email', 'Please add email').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
+    check('email', 'Please add a valid email').isEmail(),
     check('firstName', 'Please add first name').not().isEmpty(),
     check('lastName', 'Please add last name').not().isEmpty(),
-    check('password', 'Please add password').not().isEmpty(),
-    check('password', 'Please enter a password with 6 or more characteres').isLength({ min: 6 })
+    check('password', 'Please add a password with 6 or more characteres').isLength({ min: 6 })
 ], userControler.create)
 
 export default routes;
