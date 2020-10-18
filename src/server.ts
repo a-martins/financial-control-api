0e7d4ff91d15a6c763e1421424480c825790e012
Alexandre Martins <alexandremartins.ti@gmail.com>
2020-10-17 22:20:35 -0300

    Add boom middleware on express server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,20 @@
 import express from 'express'
 import cors from 'cors'
+import boom from 'express-boom'
+
 import connectDB from './config/db'
+
+import userRoutes from './routes/users'
 
 const app = express()
 
 connectDB()
 
+app.use(boom())
 app.use(cors())
 app.use(express.json())
+
+app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
